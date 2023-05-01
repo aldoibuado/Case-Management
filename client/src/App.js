fc6f0fd54d6339b9e8276cfc51d9b8e22e5617f1
aldoibuado <aldo.ibuado@gmail.com>
2023-05-01 12:00:47 -0700

    working on Navbar and added home section

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import About from './pages/About';
 import Footer from './components/Footer';
 
@@ -7,9 +10,10 @@ function App() {
   return (
     <div className="App">
       <Router>
-        
+        <Navbar />
         <Routes>
-          <Route path='/' element={<About />} />
+          <Route exact path='/' component={Home} />
+          <Route path="/about" component={About} />
 
         </Routes>
         <Footer />
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { AppBar } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: "flex",
+  },
+  logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  },
+}));
 
 function Navbar() {
+  const classes = useStyles();
+
   return (
-    <div>
-        <li>
-            <Link to="/">About</Link>
-        </li>
-    </div>
-  )
+    <AppBar position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" className={classes.logo}>
+          Nabar
+        </Typography>
+        <div className={classes.navlinks}>
+          <Link to="/" className={classes.link}>
+            Home
+          </Link>
+          <Link to="/about" className={classes.link}>
+            About Us
+          </Link>
+          <Link to="/services" className={classes.link}>
+            Services
+          </Link>
+          <Link to="/contact" className={classes.link}>
+            Contact
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Navbar;
