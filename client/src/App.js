8c9aa86a106f7d5e605cc2d53cebd2ef642201ec
aldoibuado <aldo.ibuado@gmail.com>
2023-05-04 15:24:25 -0700

    remade the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
-import Navigation from './components/Navigation';
-import './App.css';
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <Router>
-      <Navigation />
-
-      <div className="pages">
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-    </div>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </Router>
     </>
-  )
+  );
 }
 
 export default App;
