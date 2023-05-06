ae9447866e07ecefa5238e2b68f4c497da6f59d4
aldoibuado <aldo.ibuado@gmail.com>
2023-05-05 17:07:51 -0700

    working on the contact form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,12 +1,91 @@
 import React from "react";
-import '../styles/Contact.css';
-
+import {
+  Typography,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import contact from "../assets/images/Contact Us.png";
+import "../styles/Contact.css";
 
 // TODO: Make contact form and style it
-  function Contact () {
-    return (
-      <div>contact</div> 
-    );
-  }
+function Contact() {
+  return (
+    <div>
+      <Card style={{maxWidth: 450, margin:"0 auto", padding: "20px 5px"}}>
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+           Contact Us
+          </Typography>
+          <form>
+          <Grid container spacing={1}>
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="First Name"
+                placeholder="Enter first name"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} sm={6} item>
+              <TextField
+                label="Last Name"
+                placeholder="Enter last name"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                type="email"
+                label="Email"
+                placeholder="Enter email"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                type="number"
+                label="Phone"
+                placeholder="Enter phone number"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                label="Message"
+                multiline
+                minRows={4}
+                placeholder="Enter your message here"
+                variant="outlined"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
 
 export default Contact;
