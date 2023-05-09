ea4437d2ad4f87fc55a4b5d5e9357155bb42a102
aldoibuado <aldo.ibuado@gmail.com>
2023-05-09 14:56:24 -0700

    reworking the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,39 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import fidem from "../assets/images/Fidem logo.png";
-import ReorderIcon from "@material-ui/icons/Reorder";
-import "../styles/Navbar.css";
+import React from 'react';
+import fidem from '../assets/images/Fidem logo.png';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 function Navbar() {
-  const [expandNavbar, setExpandNavbar] = useState(false);
-
-  const location = useLocation();
-
-  useEffect(() => {
-    setExpandNavbar(false);
-  }, [location]);
-
   return (
-    <div className="navbar" id={expandNavbar ? "open" : "close"}>
-      <div className="toggleButton">
-        <button
-          onClick={() => {
-            setExpandNavbar((prev) => !prev);
-          }}
-        >
-          <ReorderIcon />
-        </button>
+    <div className="navbar">
+      <div className="leftSide">
+        <img src={fidem} alt="company logo" />
       </div>
-      <div className="links">
-        <Link to="/">
-          <img className="logo" src={fidem} alt="company logo" />
-        </Link>
-        <Link to="/about">About Us</Link>
-        <Link to="services">Services</Link>
-        <Link to="contact">Contact</Link>
+      <div className="rightSide">
+        <Link to="/"> Home </Link>
+        <Link to="/about"> About Us </Link>
+        <Link to="/services"> Services </Link>
+        <Link to="/contact"> Contact </Link>
       </div>
     </div>
-  );
+  )
 }
 
 export default Navbar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import photo from '../assets/images/test-banner.png';
 import '../styles/Home.css';
 
 // TODO: work on adding photos to home page and styling
@@ -7,12 +6,7 @@ function Home() {
 
   return (
     <div>
-      <h1 className="epic">Case Management!</h1>
-      <p className="epic1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-         printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
-         publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-     <img className="nurse" src={photo} alt="nurse helping patient" />
+    
     </div>
   )
 }
