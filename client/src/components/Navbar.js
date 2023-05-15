5529e1e54ede640c2e6172ff65b75184829a113a
aldoibuado <aldo.ibuado@gmail.com>
2023-05-15 16:34:25 -0700

    working on making the navbar responsive

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,39 +1,73 @@
-import React, { useState } from "react";
-import fidem from "../assets/images/Fidem logo.png";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import ReorderIcon from "@material-ui/icons/Reorder";
-import "../styles/Navbar.css";
+import { Button } from './Button';
+import '../styles/Navbar.css';
 
 function Navbar() {
-  const [openLinks, setOpenLinks] = useState(false);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-  const toggleNavbar = () => {
-    setOpenLinks(!openLinks);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
   };
 
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
   return (
-    <div className="navbar">
-      <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <Link to="/">
-        <img src={fidem} alt="company logo" />
-        </Link>
-        <div className="hiddenLinks">
-          <Link to="/"> Home </Link>
-          <Link to="/about"> About Us </Link>
-          <Link to="/services"> Services </Link>
-          <Link to="/contact"> Contact </Link>
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            Fidem <i className="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">Contact</Button>}
         </div>
-      </div>
-      <div className="rightSide">
-        {/* <Link to="/"> Home </Link> */}
-        <Link to="/about"> About Us </Link>
-        <Link to="/services"> Services </Link>
-        <Link to="/contact"> Contact </Link>
-        <button onClick={toggleNavbar}>
-          <ReorderIcon />
-        </button>
-      </div>
-    </div>
+      </nav>
+    </>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import photo from "../assets/images/nurse-helping-patient.jpg";
-import "../styles/Home.css";
+import React from 'react';
 
-// TODO: work on adding photos to home page and styling
 function Home() {
   return (
-    <div className="home"  style={{ backgroundImage: `url(${photo})` }}>
-      <div
-        className="headerContainer">
-        <h1> Fidem Case Management </h1>
-        <p>
-          Nurse Case Management
-        </p>
-        <Link to="/contact">
-          <button> Let's get started </button>
-        </Link>
-      </div>
-    </div>
-  );
+    <div>Home</div>
+  )
 }
 
 export default Home;
